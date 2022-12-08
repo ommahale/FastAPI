34861092c99779acc0a566bc7c49ce871d2e0afb
OM MAHALE <78585854+ommahale@users.noreply.github.com>
2022-12-08 18:33:30 +0530

    added db from sqlalchemy

--- blog/database.py
+++ blog/database.py
@@ -1,1 +1,11 @@
 from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABSE_URL='sqlite:///./blog.db'
+
+engine=create_engine(url=SQLALCHEMY_DATABSE_URL,connect_args={'check_same_thread':False})
+
+session=sessionmaker(bind=engine,autocommit=False,autoflush=False)
+
+Base=declarative_base()
